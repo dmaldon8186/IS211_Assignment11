7a232cc68ab70200a9549a08d436fb287f547c37
dmaldon8186 <david.maldonado36@spsmail.cuny.edu>
2023-04-23 22:07:59 -0400

    Assignment Complete

--- todoapp.py
+++ todoapp.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, redirect
+import re
 app = Flask(__name__)
 
-email_addresses = []
 task_list = []
-priority_list = []
 
 @app.route('/')
 def hello_world():
@@ -13,20 +12,25 @@ def hello_world():
 
 @app.route('/signup', methods = ['POST'])
 def signup():
-    email = request.form['email']
-    email_addresses.append(email)
-    task = request.form['task']
+    task= (request.form['email'], request.form['task'], request.form['priority'])
+    if not re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$').match(task[0]):
+        print('Email invalid')
+        return redirect('/')
+    if task[2] not in ['low', 'medium', 'high']:
+        print('Priority invalid')
+        return redirect('/')
     task_list.append(task)
-    priority = request.form['priority']
-    priority_list.append(priority)
-    print(email_addresses)
     print(task_list)
-    print(priority_list)
+    return redirect('/')
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    task_list.clear()
     return redirect('/')
 
 @app.route('/emails.html')
 def emails():
-    return render_template('emails.html', email_addresses = email_addresses, task_list = task_list, priority_list = priority_list)
+    return render_template('emails.html', task_list = task_list)
 
 if __name__ == '__main__':
     app.run()
